5f19787c1286edee5bab8c898836cb996e7abfe2
Antrax <fredcox@gmail.com>
2025-06-01 16:29:07 -0300

    ok

--- src/waydock/dock.py
+++ src/waydock/dock.py
@@ -8,7 +8,6 @@ gi.require_version("Gtk4LayerShell", "1.0")
 
 from gi.repository import Gtk  # pyright: ignore #noqa
 from gi.repository import Gtk4LayerShell as LayerShell  # pyright: ignore #noqa
-from gi.repository import GLib  # pyright: ignore # noqa
 
 from waydock.config import waydockConfig  # pyright: ignore # noqa
 from waydock.constants import STYLE_FILE, ANCHOR  # pyright: ignore # noqa
@@ -16,9 +15,67 @@ from waydock.widgets import populateBox  # pyright: ignore # noqa
 from waydock.util import printLog  # pyright: ignore # noqa
 
 
-waydockConfig = None
-components = []
-index = 0
+class DockApplication:
+    def __init__(self):
+        self.app = Gtk.Application(application_id="com.antrax.waydock")
+        self.window = None
+        self.dock_box = None
+        self.applications = []
+        self.app.connect("activate", self.on_activate)
+
+    def startGTK(self):
+        printLog("Starting GTK main loop")
+        self.window = Gtk.Window(application=self.app)
+        printLog("window created, setting properties for waydock window instance: ")
+        self.window.set_name("waydock")
+        # Carregar CSS
+        printLog("Setting up style with CSS path: " + STYLE_FILE)
+        css_provider = Gtk.CssProvider()
+        css_provider.load_from_path(f"{STYLE_FILE}")
+        display = self.window.get_display()
+        Gtk.StyleContext.add_provider_for_display(
+            display, css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        )
+        printLog("CSS provider loaded")
+
+        printLog(
+            f"bar size to '{waydockConfig.window.width}x{waydockConfig.window.height}'"  # pyright: ignore # noqa
+        )
+        window.set_default_size(waydockConfig.window.width, waydockConfig.window.height)  # pyright: ignore # noqa
+
+        printLog("Layer Shell initialized")
+        LayerShell.init_for_window(self.window)
+        LayerShell.set_layer(self.window, LayerShell.Layer.TOP)
+
+        # Anchor
+        # LayerShell.set_anchor(window, LayerShell.Edge.BOTTOM, True)
+        LayerShell.set_anchor(self.window, ANCHOR[waydockConfig.window.anchor], True)  # pyright: ignore # noqa
+        # margins
+        LayerShell.set_margin(
+            self.window,
+            LayerShell.Edge.BOTTOM,
+            waydockConfig.window.margin_bottom,  # pyright: ignore # noqa
+        )
+        LayerShell.set_margin(
+            self.window, LayerShell.Edge.TOP, waydockConfig.window.margin_top
+        )  # pyright: ignore # noqa
+
+        self.mainBox = Gtk.CenterBox(orientation=Gtk.Orientation.HORIZONTAL)
+        # faz com que todos os widgets filhos ocupem o mesmo espaço
+        # horizontalmente
+        printLog("Setting homogeneous to True for mainBox.")
+        # mainBox.set_homogeneous(False)
+
+        self.window.set_child(self.mainBox)
+
+        # Enable Exclusive Zone
+        LayerShell.auto_exclusive_zone_enable(self.window)
+
+        printLog("Show the window with all widgets.")
+        self.window.present()
+
+    def on_activate(self, app):
+        pass
 
 
 def createGtkBox(h_align: Gtk.Align) -> Gtk.Box:
@@ -94,13 +151,13 @@ def onActivate(app):
     window.present()
 
 
-def runwaydock(config: waydockConfig) -> None:
+def runWayDock(config: waydockConfig) -> None:
     """
     waydock is a GTK4 Layer Shell bar for Hyprland.
     """
-    printLog("Instantiate the config Class ")
-    global waydockConfig
-    waydockConfig = config
+    # printLog("Instantiate the config Class ")
+    # global waydockConfig
+    # waydockConfig = config
 
     # Create the application
     printLog("Create a new Application instance with 'com.antrax.waydock' as an id")
